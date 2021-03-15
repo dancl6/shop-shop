c879a9344ccda07ca47e51bb0a341a1390d32ad6
Dan Lyons <danielclyons@yahoo.com>
2021-03-15 08:39:03 -0400

    pushing to heroku

---
+++ client/build/precache-manifest.bd7c613b45305761a9f3fe3facf73e13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3faf0f7655da6f8e5374860ad1f9194d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "753c2f02714eb8df75fc",
+    "url": "/static/css/main.49b21ed8.chunk.css"
+  },
+  {
+    "revision": "8bd9632ad0f7a0542c62",
+    "url": "/static/js/2.f168f743.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.f168f743.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "753c2f02714eb8df75fc",
+    "url": "/static/js/main.2ab4ba08.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
