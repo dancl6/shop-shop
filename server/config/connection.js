811289bdb458c0a13164a429d511885bcd01f724
Dan Lyons <danielclyons@yahoo.com>
2021-03-16 22:35:28 -0400

    pushing to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,6 +5,8 @@ mongoose.connect(process.env.ATLAS_URI || 'mongodb://localhost/mernshopping', {
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
-});
+})
+.then(() => console.log( 'Database Connected' ))
+.catch(err => console.log( err ))
 
 module.exports = mongoose.connection; 
