e7a78a5afa862153b3d4f91bd6fe95ffc1f82ee1
Dan Lyons <danielclyons@yahoo.com>
2021-03-18 11:17:04 -0400

    pushing to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -9,4 +9,12 @@ mongoose.connect(process.env.ATLAS_URI || 'mongodb://localhost/mernshopping', {
 .then(() => console.log( 'Database Connected' ))
 .catch(err => console.log( err ))
 
+mongoose.connection.on("connected", () => {
+  console.log("Connect Success")
+})
+
+mongoose.connection.on("error", (err) => {
+  console.log("error", err)
+})
+
 module.exports = mongoose.connection;  
