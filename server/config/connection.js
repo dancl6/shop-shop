cfe2d623d20ab2fe94fa649505151315a443ec44
Dan Lyons <danielclyons@yahoo.com>
2021-03-18 21:10:06 -0400

    pushing to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.ATLAS_URI || 'mongodb://localhost/mernshopping', {
   useNewUrlParser: true,
-  // useUnifiedTopology: true,
+  useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
 })
