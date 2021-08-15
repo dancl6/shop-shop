a38a3cfa7e0e32fea16507ffcb77d54ad787a391
Dan Lyons <danielclyons@yahoo.com>
2021-08-14 21:19:21 -0400

    recommitting

---
+++ server/config/connection.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+require('dotenv').config()
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+})
+.then(() => console.log( 'Database Connected' ))
+.catch((error) => console.error("MongoDB connection failed:", error.message))
+
+// mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@shopshopcluster.bezyo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false
+// })
+// .then(() => {
+//   // app.listen(3000)
+//   console.log( 'Database Connected' )
+// }).catch(err => {
+//   console.log(err)
+// })
+
+
+// mongoose.connection.on("connected", () => {
+//   console.log("Connect Success")
+// })
+
+// mongoose.connection.on("error", (err) => {
+//   console.log("error", err)
+// })
+
+module.exports = mongoose.connection;  
